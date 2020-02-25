eaa0214a3f4283aae6eec951bcc658b68fd634b9
dva raz <tomzak@tomzak.ru>
2020-02-25 21:31:41 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.process1 = new System.Diagnostics.Process();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(192, 493);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(817, 485);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(210, 485);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(601, 20);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(481, 489);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Загружаюсь...";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Launcher.Properties.Resources.CN0TQofjIcY;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(904, 517);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "StalkerClassic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Diagnostics.Process process1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        const string clientid = "1";
+        const int port = 11517;
+        const string ipaddr = "127.0.0.1";
+        const string okchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,-_:";
+        
+        public string protocol { get; set; }
+        public string link { get; set; }
+        public string ftpip { get; set; }
+        public string ftpport { get; set; }
+        public string ftpuser { get; set; }
+        public string ftppwd { get; set; }
+        public string ftpfolder { get; set; }
+        public string gameip { get; set; }
+        public string gameport { get; set; }
+        public string gamepwd { get; set; }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string CleanString(string input)
+        {
+            string output = "";
+
+            foreach (char ch in input)
+            {
+                bool isCharCorrect = okchar.Contains(ch);
+                if (isCharCorrect)
+                {
+                    output += ch;
+                }
+            }
+            return output;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.AppendText($"Загрузка данных...\r\n");
+            //label1.BackColor = Color.FromArgb(1,1,1,1);
+
+            TcpClient client = new TcpClient(ipaddr, port);
+            NetworkStream stream = client.GetStream();
+            StringBuilder builder = new StringBuilder();
+
+            SendData(stream, clientid);
+
+            byte[] answerBuffer = new byte[128];
+
+            do
+            {
+                stream.Read(answerBuffer,0,answerBuffer.Length);
+                builder.Append(Encoding.Unicode.GetString(answerBuffer));
+            } while (stream.DataAvailable);
+
+            string answer = builder.ToString();
+            string[] answerArray = answer.Split(':');
+
+            answer = answerArray[1];
+            string correctAnswer = CleanString(answer);
+
+            label1.Text = correctAnswer;
+
+            bool gettingData;
+
+            switch (correctAnswer)
+            {
+                case "ok":
+                    gettingData = true;
+                    break;
+                default:
+                    gettingData = false;
+                    break;
+            }
+
+            builder.Clear();
+
+            if (gettingData)
+            {
+                label1.Text = "Начинаю загрузку";
+                FullPrepare(stream,builder);
+            }
+            else
+            {
+                label1.Text = "Отмена";
+            }
+
+        }
+        private bool IsPathesExistInReg() 
+        {
+            bool pathesOk = true;
+
+
+
+            return pathesOk;
+        }
+        private void FullPrepare(NetworkStream stream, StringBuilder builder)
+        {
+            byte[] answerBuffer = new byte[16];
+
+            do
+            {
+                stream.Read(answerBuffer, 0, answerBuffer.Length);
+                builder.Append(Encoding.Unicode.GetString(answerBuffer));
+            } while (stream.DataAvailable);
+
+            string answer = CleanString(builder.ToString());
+            string[] answerArray = answer.Split(':');
+
+            //textBox1.AppendText($"\r\n{answer}");
+
+            protocol = answerArray[0];
+            link = answerArray[1];
+            ftpip = answerArray[2];
+            ftpport = answerArray[3];
+            ftpuser = answerArray[4];
+            ftppwd = answerArray[5];
+            ftpfolder = answerArray[6];
+            gameip = answerArray[7];
+            gameport = answerArray[8];
+            gamepwd = answerArray[9];
+
+            textBox1.AppendText($"\r\n{protocol}");
+            textBox1.AppendText($"\r\n{link}");
+            textBox1.AppendText($"\r\n{ftpip}");
+            textBox1.AppendText($"\r\n{ftpport}");
+            textBox1.AppendText($"\r\n{ftpuser}");
+            textBox1.AppendText($"\r\n{ftppwd}");
+            textBox1.AppendText($"\r\n{ftpfolder}");
+            textBox1.AppendText($"\r\n{gameip}");
+            textBox1.AppendText($"\r\n{gameport}");
+            textBox1.AppendText($"\r\n{gamepwd}");
+
+
+        }
+        private void SendData(NetworkStream stream, string info)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(info);
+            stream.Write(data, 0, data.Length);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+/*
+ StringBuilder StrBuilder = new StringBuilder();
+
+            byte[] data = Encoding.Unicode.GetBytes(clientid);
+            byte[] buffer = new byte[256];
+
+            stream.Write(data, 0, data.Length);
+
+            stream.Flush();
+
+            int incoming = 0;
+            byte[] incomingData = new byte[256];
+            while (stream.DataAvailable)
+            {
+                incoming = stream.Read(buffer, 0, buffer.Length);
+                string str = Encoding.Unicode.GetString(incomingData, 0, incoming);
+                StrBuilder.Append(str);
+            }
+            textBox1.Text += StrBuilder.ToString()+"\r\n";* 
+
+
+           private string GetData(NetworkStream stream)
+        {
+            string result;
+
+            StringBuilder StrBuilder = new StringBuilder();
+
+            int bytes;
+            byte[] data = new byte[256];
+
+            do
+            {
+                bytes = stream.Read(data, 0, data.Length);
+                StrBuilder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+            } while (stream.DataAvailable);
+
+            result = StrBuilder.ToString();
+
+            return result;
+        }
+
+ */
